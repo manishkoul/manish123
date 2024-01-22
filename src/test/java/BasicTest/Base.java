d4b89fdaf901481becd8bb7cfc1dd833dbd508e6
manishkoul99 <manish_koul@epam.com>
2024-01-22 15:14:51 +0530

    sss

--- src/test/java/BasicTest/Base.java
+++ src/test/java/BasicTest/Base.java
@@ -1,4 +1,4 @@
-package java;
+package BasicTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/test/java/BasicTest/LocatorStrategyExample.java
+++ src/test/java/BasicTest/LocatorStrategyExample.java
@@ -1,4 +1,4 @@
-package java;
+package BasicTest;
 
 import org.openqa.selenium.By;
 
